024681f5f6a184d5fadf682ac7ef6134a323c28f
Raimundas <raimundas.banikonis@itvision.lt>
2021-10-27 15:58:14 +0300

    commented environment.json

--- core/settings.py
+++ core/settings.py
@@ -18,9 +18,9 @@ import json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-json_file_path = os.path.join(BASE_DIR, "environment.json")
-with open(json_file_path) as file:
-    env = json.load(file)
+# json_file_path = os.path.join(BASE_DIR, "environment.json")
+# with open(json_file_path) as file:
+#     env = json.load(file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
